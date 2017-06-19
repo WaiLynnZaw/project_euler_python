bc917a798c1e7eee6eb8c7d7a17d78a0a8afdc0d
Wai Lynn Zaw <wailynnzaw.lamz@gmail.com>
2017-06-19 15:26:31 +0630

    Finish up problem 22

---
+++ Problem22.py
@@ -0,0 +1,14 @@
+f = open("p22_names.txt", 'r')
+char_num_dict = {
+    "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14,
+    "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26
+}
+names = sorted(f.read().replace('"', '').split(','), key=str)
+result = 0
+for key, val in enumerate(names):
+    temp = 0
+    for x in val:
+        temp += char_num_dict[x]
+    result += temp * (key + 1)
+
+print result
